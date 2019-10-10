188f16f7c69c828540894ffc95968183e48de79d
sweos <atorsb@gmail.com>
2019-10-10 08:42:24 +0200

    Hello there

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+        System.out.println("General Kenobi....You are a bold one");
+    }
 }
